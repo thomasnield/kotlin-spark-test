c84a2666bbd972874c66893c688a4f5cabd70242
Thomas Nield <thomasnield@live.com>
2016-10-09 17:51:27 -0500

    add kryo serialization

--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -1,17 +1,18 @@
 
 import org.apache.spark.SparkConf
 import org.apache.spark.api.java.JavaSparkContext
-import java.io.Serializable
 
 fun main(args: Array<String>) {
+
+    class MyItem(val id: Int, val value: String)
 
     val conf = SparkConf()
             .setMaster("local")
             .setAppName("Line Counter")
 
-    val sc = JavaSparkContext(conf)
+    conf.registerKryoClasses(arrayOf(MyItem::class.java))
 
-    class MyItem(val id: Int, val value: String): Serializable
+    val sc = JavaSparkContext(conf)
 
     val input = sc.parallelize(listOf(MyItem(1,"Alpha"),MyItem(2,"Beta")))
 
